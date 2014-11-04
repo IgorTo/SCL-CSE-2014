a373f3f2fdfd4ac897e0e7376a17299efee6b599
IgorTo <igor.tominec@gmail.com>
2014-11-04 14:51:51 +0100

    bla

--- ODE2/Newton.m
+++ ODE2/Newton.m
@@ -1,13 +1,16 @@
-function x1 = Newton( f, df, x0 )
+function x1 = Newton( f, df, x0, eps )
 %NEWTON A function for solving the equation F(x)=0 by Newton's method.
 %   Usage: Newton(f, df, x0)
-%   f ... left hand-side function F(x)
-%   df ... the derivative of left hand-side function F'(x)
+%   f ... left hand-side function F(x), should be given by anonymous
+%   function
+%   df ... the derivative of left hand-side function F'(x), should be given
+%   by anonymous function
 %   x0 ... initial (guess) value
+%   eps
 
 while(true)
     x1=x0-f(x0)/df(x0);
-    if (abs(x1-x0) < 1e-5) break; end
+    if (abs(x1-x0) < eps) break; end
     x0=x1;
 end
 end
