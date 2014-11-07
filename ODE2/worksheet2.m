f5016a5366a8227926b55ac87e44c67dc5f6a7b9
JuanCarlosCSE <medina.juancarlos@gmail.com>
2014-11-07 15:46:52 +0100

    Adams methods and respective error tables

--- ODE2/worksheet2.m
+++ ODE2/worksheet2.m
@@ -26,22 +26,59 @@ axis([t0 tend 0 20]); xlabel('time'); ylabel('population'); title('Explicit Eule
 figure(3); plot(t, p_ref(t), sprintf('%c',colors(1))); 
 axis([t0 tend 0 20]); xlabel('time'); ylabel('population'); title('Heun');
 
+% The Adams figure
+figure(4); plot(t, p_ref(t), sprintf('%c',colors(1))); 
+axis([t0 tend 0 20]); xlabel('time'); ylabel('population'); title('Adams');
+
+% The AdamsL1 figure
+figure(5); plot(t, p_ref(t), sprintf('%c',colors(1))); 
+axis([t0 tend 0 20]); xlabel('time'); ylabel('population'); title('AdamsL1');
+
+% The AdamsL2 figure
+figure(6); plot(t, p_ref(t), sprintf('%c',colors(1))); 
+axis([t0 tend 0 20]); xlabel('time'); ylabel('population'); title('AdamsL1');
+
+
 % Solving the ODE and plotting its solution for different dt
 for i=1:6
     [p, time]=Euler(p_der, dt, p0, tend);
     figure(2); hold on; plot(time, p, sprintf('%c', colors(i+1)));
     errorEuler(i)=sqrt(sum((p_ref(t0:dt:tend)-p).^2).*dt./5);
     
+    
+    
     [p, time]=Heun(p_der, dt, p0, tend);
+    time
     figure(3); hold on; plot(time, p, sprintf('%c',colors(i+1)));
     errorHeun(i)=sqrt(sum((p_ref(t0:dt:tend)-p).^2).*dt./5);
+    
+    
+     
+    [p,time]=Adams(p_der, dt, p0, tend);
+    
+    figure(4); hold on; plot(time, p, sprintf('%c',colors(i+1)));
+    errorAdams(i)=sqrt(sum((p_ref(t0:dt:tend)-p).^2).*dt./5);
+    
+   
+    [p,time]=AdamsL1(p_der, dt, p0, tend);
+    figure(5); hold on; plot(time, p, sprintf('%c',colors(i+1)));
+    errorAdamsL1(i)=sqrt(sum((p_ref(t0:dt:tend)-p).^2).*dt./5);
+   
+    [p,time]=AdamsL2(p_der, dt, p0, tend);
+    
+    figure(6); hold on; plot(time, p, sprintf('%c',colors(i+1)));
+    errorAdamsL2(i)=sqrt(sum((p_ref(t0:dt:tend)-p).^2).*dt./5);
     dt=dt/2;
 end
 
 % The Plot Legend configuration
 figure(2); legend('Analytic', 'dt=1', 'dt=1/2', 'dt=1/4', 'dt=1/8', 'dt=1/16', 'dt=1/32', 'Location', 'SouthEast');
 figure(3); legend('Analytic', 'dt=1', 'dt=1/2', 'dt=1/4', 'dt=1/8', 'dt=1/16', 'dt=1/32', 'Location', 'SouthEast');
-close all
+figure(4); legend('Analytic', 'dt=1', 'dt=1/2', 'dt=1/4', 'dt=1/8', 'dt=1/16', 'dt=1/32', 'Location', 'SouthEast');
+figure(5); legend('Analytic', 'dt=1', 'dt=1/2', 'dt=1/4', 'dt=1/8', 'dt=1/16', 'dt=1/32', 'Location', 'SouthEast');
+figure(6); legend('Analytic', 'dt=1', 'dt=1/2', 'dt=1/4', 'dt=1/8', 'dt=1/16', 'dt=1/32', 'Location', 'SouthEast');
+
+
 
 %H)
 
@@ -59,7 +96,9 @@ for i=1:5
          errorHeun_Red(i)=NaN;
     end
    
-    %To include reduced errors for new methods
+    errorAdams_Red(i)=errorAdams(i)/errorAdams(i+1);
+    errorAdamsL1_Red(i)=errorAdamsL1(i)/errorAdamsL1(i+1);
+    errorAdamsL2_Red(i)=errorAdamsL2(i)/errorAdamsL2(i+1);
 end
 
 
@@ -89,5 +128,27 @@ t = uitable(tab2,'Data',data,'ColumnName',cnames,'RowName',rnames);
 t.Position(3) = t.Extent(3);
 t.Position(4) = t.Extent(4);
 
-%To include tables for new methods
+tab3 = uitab(tabgp,'Title','Adams');
+data = [errorAdams(2:6); errorAdams_Red(1:5)];
+% Create the uitable
+t = uitable(tab3,'Data',data,'ColumnName',cnames,'RowName',rnames);
+% Set width and height
+t.Position(3) = t.Extent(3);
+t.Position(4) = t.Extent(4);
+
+tab4 = uitab(tabgp,'Title','AdamsL1');
+data = [errorAdamsL1(2:6); errorAdamsL1_Red(1:5)];
+% Create the uitable
+t = uitable(tab4,'Data',data,'ColumnName',cnames,'RowName',rnames);
+% Set width and height
+t.Position(3) = t.Extent(3);
+t.Position(4) = t.Extent(4);
+
+tab5 = uitab(tabgp,'Title','AdamsL2');
+data = [errorAdamsL2(2:6); errorAdamsL2_Red(1:5)];
+% Create the uitable
+t = uitable(tab5,'Data',data,'ColumnName',cnames,'RowName',rnames);
+% Set width and height
+t.Position(3) = t.Extent(3);
+t.Position(4) = t.Extent(4);
 
