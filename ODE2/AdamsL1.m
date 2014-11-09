58c9f5f9177689afb5d003305bfd3cb475f35de5
IgorTo <igor.tominec@gmail.com>
2014-11-09 19:20:19 +0100

    Added the function description.

--- ODE2/AdamsL1.m
+++ ODE2/AdamsL1.m
@@ -1,5 +1,11 @@
-function res=AdamsL1(dt, y0, tend)
+% ADAMSL1(dt, y0, tend) solves the ODE equation of p'(t)=(1-p(t)/10)*7p(t)
+%	The solver produces the results via the first linearised version of Adams-Moulton's method.
+%	Usage: AdamsL1(dt, y0, tend)
+%	dt ... time step
+%	y0 ... initial value at time=0
+%	tend ... the end time of simulation
 
+function res=AdamsL1(dt, y0, tend)
 
 res=zeros(1, tend/dt+1);
 res(1)=y0; % Initial value.
